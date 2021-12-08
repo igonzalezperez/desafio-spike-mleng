ad165bbe1684c56772e02342634478eaf26bbffb
igonzalezperez <ivan.gonzalez@ug.uchile.cl.com>
2021-12-08 13:53:44 -0300

    feat: A .csv to database.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from src.preprocess.preprocess import preprocess
-from src.training.grid_search import grid_search
+from src.preprocessing import create_db
+from src.training import grid_search, train
+from src.predict import make_prediction
+
+
 if __name__ == '__main__':
-    preprocess()
+    # create_db(mode='replace')
+    # preproc()
+    # grid_search()
+    # train()
+    # make_prediction('2020-06')
+    pass
